7a79fb9cf47ec0017681bbd7477c78df4dcb49b5
Soluna <julia3.williamson@gmail.com>
2025-01-07 15:57:55 -0600

    Regex Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,10 @@
+use regex::Regex;
 use std::io;
 
 const PRECISION: usize = 2;
-
-fn convert_temperature(temp: f64, unit: Option<char>) {
-    let unit = unit.expect("Not a valid unit");
-    let kelvin = match unit {
-        'k' => temp,
-        'c' => temp + 273.15,
-        'r' => temp / 1.8,
-        'f' => (temp - 32.0) / 1.8 + 273.15,
-        _ => return (),
-    };
-    let celcius = match unit {
-        'k' => temp - 273.15,
-        'c' => temp,
-        'r' => temp * 1.8 + 491.67,
-        'f' => (temp - 32.0) / 1.8,
-        _ => return (),
-    };
-    let rankine = match unit {
-        'k' => temp * 1.8,
-        'c' => (temp - 491.67) / 1.8,
-        'r' => temp,
-        'f' => temp + 459.67,
-        _ => return (),
-    };
-    let farenheit = match unit {
-        'k' => (temp - 273.15) * 1.8 + 32.0,
-        'c' => temp * 1.8 + 32.0,
-        'r' => temp - 459.67,
-        'f' => temp,
-        _ => return (),
-    };
-    println!("{:>16.1$}°C", celcius, PRECISION);
-    println!("{:>16.1$}°F", farenheit, PRECISION);
-    println!("{:>16.1$}°K", kelvin, PRECISION);
-    println!("{:>16.1$}°R", rankine, PRECISION);
-}
-
 fn main() {
     loop {
-        println!("==================");
+        println!("===================");
 
         println!("Enter Temperature:");
 
@@ -51,19 +15,24 @@ fn main() {
             .expect("Failed to read line");
 
         let unit = if input.contains("k") || input.contains("K") {
-            Some('k')
+            Some('K')
         } else if input.contains("c") || input.contains("C") {
-            Some('c')
+            Some('C')
         } else if input.contains("r") || input.contains("R") {
-            Some('r')
+            Some('R')
         } else if input.contains("f") || input.contains("F") {
-            Some('f')
+            Some('F')
         } else {
             {
                 println!("Use valid units!");
                 continue;
             }
         };
+
+        let sign: f64 = if input.contains('-') { -1.0 } else { 1.0 };
+
+        let pattern = Regex::new(r"[^\d.]").unwrap();
+        let input = pattern.replace_all(&input, "");
 
         let temp: f64 = match input.trim().parse() {
             Ok(num) => num,
@@ -73,34 +42,55 @@ fn main() {
             }
         };
 
-        println!("------------------");
+        let temp = temp * sign;
+        let unit_symbol = unit.unwrap();
+
+        println!("-------------------");
+
+        println!("{temp}°{unit_symbol} =");
+
+        println!("* - - - - - - - - *");
 
         convert_temperature(temp, unit);
     }
 }
 
-//fn number_maker(input: String) {
-//     let mut decimals: u32 = 0;
-//     let mut num: f64 = if input.contains("-") { -0.0 } else { 0.0 };
-//     for i in 0..input.len() - 1 {
-//         println!("Loop {i}: {num}");
-//         if input.chars().nth(i).expect("Char error").is_digit(10) == true {
-//             let digit = input.chars().nth(i).expect("Not a digit").to_digit(10);
-//             let digit = digit.unwrap_or(0) + 1;
-//             num = num * 10.0 + digit as f64;
-//             match decimals {
-//                 0 => continue,
-//                 _ => decimals += 1,
-//             }
-//             continue;
-//         } else if input.chars().nth(i) == Some('.') && decimals == 0 {
-//             decimals = 1;
-//             continue;
-//         } else {
-//             continue;
-//         }
-//     }
-//     let decimals = 10_u64.pow(decimals);
-//     num = num / decimals as f64;
-//     println!("{num}");
-// }
+fn convert_temperature(temp: f64, unit: Option<char>) {
+    let unit = unit.expect("Not a valid unit");
+    let kelvin = match unit {
+        'K' => temp,
+        'C' => temp + 273.15,
+        'R' => temp / 1.8,
+        'F' => (temp - 32.0) / 1.8 + 273.15,
+        _ => return (),
+    };
+    let celcius = match unit {
+        'K' => temp - 273.15,
+        'C' => temp,
+        'R' => temp * 1.8 + 491.67,
+        'F' => (temp - 32.0) / 1.8,
+        _ => return (),
+    };
+    let rankine = match unit {
+        'K' => temp * 1.8,
+        'C' => (temp - 491.67) / 1.8,
+        'R' => temp,
+        'F' => temp + 459.67,
+        _ => return (),
+    };
+    let farenheit = match unit {
+        'K' => (temp - 273.15) * 1.8 + 32.0,
+        'C' => temp * 1.8 + 32.0,
+        'R' => temp - 459.67,
+        'F' => temp,
+        _ => return (),
+    };
+    if kelvin < 0.0 {
+        println!("INVALID TEMPERATURE");
+        println!("* - - - - - - - - *");
+    }
+    println!("{:>17.1$}°C", celcius, PRECISION);
+    println!("{:>17.1$}°F", farenheit, PRECISION);
+    println!("{:>17.1$}°K", kelvin, PRECISION);
+    println!("{:>17.1$}°R", rankine, PRECISION);
+}
